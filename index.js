3a160ee81c6b8d5db66280e054d9d8cc9a854140
Keith Brosch <Keith.Brosch@wayne.edu>
2021-07-15 17:31:50 -0400

    basic script

---
+++ index.js
@@ -0,0 +1,32 @@
+// this script should be run once per day to get any new teams on the vlr.gg rankings page
+const cheerio = require('cheerio');
+const request = require('request');
+
+const url = 'https://www.vlr.gg/rankings';
+
+let teams = [];
+    request(url, (error, response) => {
+        if (error) {
+            console.log(error);
+        } else {
+            const $ = cheerio.load(response.body);
+            const teamDivs = $('.rank-item-team');
+            const numTeams = teamDivs.length;
+            let count = 0;
+            while (count < numTeams && numTeams !== 0) {
+                const teamName = Object.values(teamDivs[count].attribs)[0];
+                let teamURL = Object.values(teamDivs[count].children[1].attribs)[0];
+                const teamID = teamURL.split('/')[2];
+                const teamThumbnail = Object.values(teamDivs[count].children[1].children[1].attribs)[0];
+                count += 1;
+                const team = {
+                    id: teamID,
+                    name: teamName,
+                    thumbnail: teamThumbnail,
+                }
+                teams.push(team);
+            }
+        }
+        console.log(teams);
+        // make an axios call to store these teams in the DB if they aren't already in it
+    });
